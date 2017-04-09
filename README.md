568a82214fee99fa103c32e11b893bf2541bb3df
Supriyo Biswas <supriyo.biswas.96@gmail.com>
2017-04-10 01:07:22 +0530

    Initial commit.

---
+++ README.md
@@ -0,0 +1,20 @@
+# Elbo Chrome Extension/WebExtension
+
+Chrome Extension/WebExtension for use with [elbo.in](https://elbo.in/), the URL shortening service. You can install it in your your browser from:
+
+* Chrome: https://chrome.google.com/webstore/detail/elboin-url-shortener/ecllbibphdcbbpjdilliapdjmdneoacp
+* Firefox: https://addons.mozilla.org/en-GB/firefox/addon/elbo-url-shortener/
+
+# License
+
+Copyright (C) 2017 <admin@booleanworld.com>
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+This project uses [FontAwesome icons](http://fontawesome.io), and the terms are available [here](http://fontawesome.io/license/).
+
+In addition, specific files in the source may have their own licensing terms.
---
+++ js/app.js
@@ -0,0 +1,105 @@
+var clipboard = new Clipboard('.copy-button');
+
+clipboard.on("success", function(e) {
+	var jqelement = $(e.trigger);
+	jqelement.attr("title", "Copied!").tooltip("show");
+	setTimeout(function() {
+		jqelement.attr("title", "").tooltip("destroy");
+	}, 800);
+});
+
+chrome.tabs.query({
+	"active": true,
+	"lastFocusedWindow": true
+}, function (tabs) {
+	var tab_url = tabs[0].url;
+	var result_area = $("#result-area");
+
+	if (!/^https?:\/\/[^/]+/.test(tab_url)) {
+		result_area.append($("<div/>", {
+			"class": "well message-well centered-text"
+		}).text("This link cannot be shortened."));
+
+		return;
+	}
+
+	result_area.append($("<div/>", {
+		"class": "well message-well centered-text"
+	}).html('<i class="icon-arrows-cw animate-spin"></i> Crunching your link'));
+
+	$.post("https://elbo.in/~shorten", {
+		"url": tab_url
+	}).done(function(data) {
+		result_area.empty();
+
+		if (data.status) {
+			var fullshorturl = "https://elbo.in/" + data.shorturl;
+			var enc_fullshorturl = encodeURIComponent(fullshorturl);
+			var shorturl = "elbo.in/" + data.shorturl;
+
+			result_area.append($("<h4/>", {
+				"class": "centered-text short-link well"
+			}).append($("<a/>", {
+				"href": fullshorturl,
+				"target": "_blank"
+			}).text(shorturl)));
+
+			var actions_row = $("<div/>", {
+				"class": "row"
+			});
+
+			var share_col = $("<div/>", {
+				"class": "col-xs-6 share-icons"
+			});
+
+			share_col.append($("<a/>", {
+				"href": "https://www.facebook.com/sharer.php?u=" + enc_fullshorturl,
+				"class": "icon-facebook-squared",
+				"target": "_blank"
+			}));
+
+			share_col.append($("<a/>", {
+				"href": "https://twitter.com/intent/tweet?url=" + enc_fullshorturl,
+				"class": "icon-twitter-squared",
+				"target": "_blank"
+			}));
+
+			share_col.append($("<a/>", {
+				"href": "https://plus.google.com/share?url=" + enc_fullshorturl,
+				"class": "icon-gplus-squared",
+				"target": "_blank"
+			}));
+
+			var copy_col = $("<div/>", {
+				"class": "col-xs-6"
+			}).append($("<div/>", {
+				"data-clipboard-text": fullshorturl,
+				"class": "btn btn-primary col-xs-12 copy-button"
+			}).html('<i class="icon-clone"></i> Copy link'));
+
+			actions_row.append(share_col).append(copy_col);
+			result_area.append(actions_row).append($("<div/>", {
+				"class": "centered-text promo-text"
+			}).html('For analytics and custom links, visit <a href="https://elbo.in/">elbo.in</a>'));
+		}
+		else if (data.reason === "invalid_url") {
+			result_area.append($("<div/>", {
+				"class": "well message-well centered-text"
+			}).text("This link cannot be shortened."));
+		}
+		else if (data.reason === "ratelimited") {
+			result_area.append($("<div/>", {
+				"class": "well message-well centered-text"
+			}).text('You have shortened too many URLs for now. Please visit <a href="https://elbo.in">elbo.in</a> to shorten more :)'));
+		}
+		else {
+			result_area.append($("<div/>", {
+				"class": "well message-well centered-text"
+			}).text('Oops! Shortening that URL failed due to an unexpected error. If the problem persists, please <a href="https://www.booleanworld.com/contact-us\">contact us</a>.'));
+		}
+	}).fail(function() {
+		result_area.append($("<div/>", {
+				"class": "well message-well centered-text"
+		}).text('Oops! Shortening that URL failed due to an unexpected error. If the problem persists, please <a href="https://www.booleanworld.com/contact-us">contact us</a>.'));
+	});
+});
